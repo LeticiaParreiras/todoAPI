b32cc7ff55773562b35d83d62cba1581a47e120f
Leticia Parreiras <leticia.x.parreiras@gmail.com>
2025-06-08 22:12:38 -0300

    adding auth rotes

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,15 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from 'src/user/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Task {
-    @PrimaryGeneratedColumn()
-    id: number
-      @Column()
-      name: string;
-    
+  @PrimaryGeneratedColumn()
+  id: number;
+  
+  @Column()
+  name: string;
+
   @Column({ nullable: true })
   description?: string;
 
   @Column({ nullable: true })
   date?: string;
+
+  @ManyToOne(() => User, (user) => user.tasks)
+  user: User;
 }
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,20 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, UseGuards, Req } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.taskService.create(createTaskDto);
+  create(@Body() createTaskDto: CreateTaskDto, @Req() req: any) {
+    return this.taskService.create(createTaskDto, req.user.userId);
   }
 
   @Get()
-  async findAll() {
-    const task = await this.taskService.findAll();
+  async findAll(@Req() req: any) {
+    const task = await this.taskService.findAll(req.user.userId);
     if(!task) return ("No task created")
       return (task)
   }
@@ -27,16 +29,17 @@ export class TaskController {
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    const task = await this.taskService.update(+id, updateTaskDto);
+  async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto, @Req() req: any) {
+    const task = await this.taskService.update(+id, req.user.userId ,updateTaskDto);
     if(!task) throw new NotFoundException();
     return (task)
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    const task = await this.taskService.remove(+id);
+  async remove(@Param('id') id: string, @Req() req: any) {
+    const task = await this.taskService.remove(+id, req.user.userId);
     if(!task) throw new NotFoundException();
     return ("Task delete")
   }
 }
+
